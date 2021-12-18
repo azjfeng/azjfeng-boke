319d261a94d9516f1cc76eaca4fb86c48735061b
jameinfeng <jameinfeng@tencent.com>
2021-12-18 15:12:31 +0800

    添加tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  purge: [
+    './template/*.html',
+    './src/**/*.tsx',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+}
