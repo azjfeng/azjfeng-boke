b09e4353b8d3888abb95f9f8fa1c3068298299a9
jameinfeng <jameinfeng@tencent.com>
2021-12-28 10:23:19 +0800

    增加阿里图标

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,33 @@
-import React  from "react";
-import "../styles/index.css";
+import React from "react";
+import "../styles/index.less";
 import "../styles/tailwind.css";
-
-
+import Header from './header/Header';
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    IndexRoute
+} from "react-router-dom";
 interface ItemPropsValue {
     title: string;
 }
-const Item = (props: ItemPropsValue)=>{
+const Item = (props: ItemPropsValue) => {
     return <span className="text-gray-100 iconfont icon-browse">{props.title}</span>
 }
-function App(){
-    const arr: any[] = [1,2,3,4,5]
-    return <div  className="h-16 bg-gray-500 flex flex-row">
-        {
-            arr.map((item)=>{
-                return <Item title={item}/>
-            })
-        }
-        <span className="text-gray-100  iconfont icon-favorite"></span>
+function App() {
+    const arr: any[] = [1, 2, 3, 4, 5]
+    return <div className="h-16">
+        <Router>
+            <Route path="/" component={Header} >
+            </Route>
+            {/* <Route exact path="/edit" component={Edit} >
+            </Route>
+            <Route exact path="/nav" component={Nav} >
+            </Route>
+            <Route exact path="/reedit/:title" component={ReEdit} >
+            </Route> */}
+        </Router>
     </div>
 }
 
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,65 @@
+import React from 'react'
+// import '../../style/header/header.less'
+// import Home from '../home/Home.tsx';
+// import Detail from '../detail/Detail.tsx'
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+class Header extends React.Component<IProps, IState>{
+    constructor(props) {
+        super(props)
+        this.state = {
+            navList: [
+                { id: 1, value: "博客首页", path: "/" },
+                { id: 2, value: "技术分享", path: "/todo" },
+                { id: 3, value: "程序人生", path: "/todo" },
+                { id: 4, value: "博客推荐", path: "/todo" },
+                { id: 5, value: "关于我", path: "/todo" },
+                { id: 6, value: "留言", path: "/todo" },
+                { id: 7, value: "个人博客模板", path: "/todo" },
+            ],
+            hashName:""
+        }
+    }
+    componentDidMount(){
+        console.log(location.hash)
+        window.addEventListener('hashchange',()=>{
+            console.log(location.hash)
+
+        },false)
+    }
+    componentDidUpdate(){
+
+    }
+    render() {
+        return (
+            <Router>
+                <div className="header">
+                    <div className="header_nav">
+                        <div className="author">
+                            <span>jameinfeng</span>
+                        </div>
+                        <div className="nav_btn">
+                            <ul>
+                                {
+                                    this.state.navList.map((item, key) => {
+                                        return <li><Link to={item.path} key={item.key}> {item.value}</Link></li>
+                                    })
+                                }
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                {/* <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/detail/:title" component={Detail} />
+                </Switch> */}
+            </Router>
+        )
+    }
+}
+
+export default Header
